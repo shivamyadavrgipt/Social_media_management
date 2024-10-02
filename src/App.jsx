62cbf41c750d504965caf25c17906ecb680901c8
Sowmik Dey <sowmikdey23@gmail.com>
2024-10-03 01:57:02 +0530

    settings file name has been renamed here s is capitalized

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Dashboard from "./pages/Dashboard";
 import Posts from "./pages/Posts";
 import Analytics from "./pages/Analytics";
 import Navbar from "./components/Navbar";
+import Settings from "./pages/Settings"
 import './App.css'
 import Footer from "./components/Footer";
  // Ensure this path is correct
@@ -19,7 +20,7 @@ function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="/posts" element={<Posts />} />
           <Route path="/analytics" element={<Analytics />} />
-          <Route path="/settings" element={<Settings />} />
+          <Route path="/settings" element={<Settings/>} />
         </Routes>
       </div>
       <Footer/>
