2cf0f0d671a4a26a9a5c2720f30db9f7c08bf3cc
ritikprajapat21 <ritikprajapati084@gmail.com>
2024-10-16 14:38:41 +0530

    Fixed page reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-// App.jsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Posts from "./pages/Posts";
 import Analytics from "./pages/Analytics";
 import Navbar from "./components/Navbar";
-import './App.css'
+import "./App.css";
 import Footer from "./components/Footer";
-import Settings from './pages/Settings'; // Ensure this path is correct
-import LandingPage from './pages/LandingPage'; // Ensure this path is correct
+import Settings from "./pages/settings";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <div className="body-content"> 
+      <div className="body-content">
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
@@ -23,30 +22,68 @@ function App() {
           <Route path="/settings" element={<Settings />} />
         </Routes>
       </div>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
 
 export default App;
 
-
 <div align="center">
   <p>
     <a href="https://github.com/shivamyadavrgipt/Social_media_management">
-      <img src="https://badges.frapsoft.com/os/v1/open-source.svg?v=103" alt="Open Source Love svg1" />
+      <img
+        src="https://badges.frapsoft.com/os/v1/open-source.svg?v=103"
+        alt="Open Source Love svg1"
+      />
     </a>
-    <img src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat" alt="PRs Welcome" />
-    <img src="https://api.visitorbadge.io/api/visitors?path=sk66641%2FAquaGuardians&countColor=%23263759&style=flat" alt="Visitors" />
-    <img src="https://img.shields.io/github/forks/shivamyadavrgipt/Social_media_management" alt="GitHub forks" />
-    <img src="https://img.shields.io/github/stars/shivamyadavrgipt/Social_media_management" alt="GitHub Repo stars" />
-    <img src="https://img.shields.io/github/contributors/shivamyadavrgipt/Social_media_management" alt="GitHub contributors" />
-    <img src="https://img.shields.io/github/last-commit/shivamyadavrgipt/Social_media_management" alt="GitHub last commit" />
-    <img src="https://img.shields.io/github/repo-size/shivamyadavrgipt/Social_media_management" alt="GitHub repo size" />
-    <img src="https://img.shields.io/github/license/shivamyadavrgipt/Social_media_management" alt="GitHub license" />
-    <img src="https://img.shields.io/github/issues/shivamyadavrgipt/Social_media_management" alt="GitHub issues" />
-    <img src="https://img.shields.io/github/issues-closed-raw/shivamyadavrgipt/Social_media_management" alt="GitHub closed issues" />
-    <img src="https://img.shields.io/github/issues-pr/shivamyadavrgipt/Social_media_management" alt="GitHub pull requests" />
-    <img src="https://img.shields.io/github/issues-pr-closed/shivamyadavrgipt/Social_media_management" alt="GitHub closed pull requests" />
+    <img
+      src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat"
+      alt="PRs Welcome"
+    />
+    <img
+      src="https://api.visitorbadge.io/api/visitors?path=sk66641%2FAquaGuardians&countColor=%23263759&style=flat"
+      alt="Visitors"
+    />
+    <img
+      src="https://img.shields.io/github/forks/shivamyadavrgipt/Social_media_management"
+      alt="GitHub forks"
+    />
+    <img
+      src="https://img.shields.io/github/stars/shivamyadavrgipt/Social_media_management"
+      alt="GitHub Repo stars"
+    />
+    <img
+      src="https://img.shields.io/github/contributors/shivamyadavrgipt/Social_media_management"
+      alt="GitHub contributors"
+    />
+    <img
+      src="https://img.shields.io/github/last-commit/shivamyadavrgipt/Social_media_management"
+      alt="GitHub last commit"
+    />
+    <img
+      src="https://img.shields.io/github/repo-size/shivamyadavrgipt/Social_media_management"
+      alt="GitHub repo size"
+    />
+    <img
+      src="https://img.shields.io/github/license/shivamyadavrgipt/Social_media_management"
+      alt="GitHub license"
+    />
+    <img
+      src="https://img.shields.io/github/issues/shivamyadavrgipt/Social_media_management"
+      alt="GitHub issues"
+    />
+    <img
+      src="https://img.shields.io/github/issues-closed-raw/shivamyadavrgipt/Social_media_management"
+      alt="GitHub closed issues"
+    />
+    <img
+      src="https://img.shields.io/github/issues-pr/shivamyadavrgipt/Social_media_management"
+      alt="GitHub pull requests"
+    />
+    <img
+      src="https://img.shields.io/github/issues-pr-closed/shivamyadavrgipt/Social_media_management"
+      alt="GitHub closed pull requests"
+    />
   </p>
-</div>
+</div>;
