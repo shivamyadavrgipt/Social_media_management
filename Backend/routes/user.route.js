f35d41d1af714ffe698edea0a51f3661f5ce1a1a
[Shivam Yadav] <shivamyadav201204@gmail.com>
2024-10-21 21:25:26 +0530

    updated

--- Backend/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { registerUser, loginUser } from '../controllers/user.controller.js';
-
-const router = express.Router();
-
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-
-export default router;
--- Backend/utils/upload.js
+++
@@ -1,7 +0,0 @@
-import multer from 'multer';
-import { storage } from './cloud.js'; // ESM-style import
-
-// Initialize Multer with Cloudinary storage
-const upload = multer({ storage });
-
-export default upload;
